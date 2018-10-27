8d2e0146c21d3f2b6cfa1c48d1f6bd016b109115
samy <samy@DESKTOP-5QRKJ7I>
2018-10-27 22:00:40 +0200

    Initial Commit GUI

---
+++ TSPACOGUI/TSPACOGUI/MainFrame.cpp
@@ -0,0 +1,14 @@
+#include "MainFrame.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	TSPACOGUI::MainFrame form;
+	Application::Run(%form);
+}
---
+++ TSPACOGUI/TSPACOGUI/MainFrame.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace TSPACOGUI {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Zusammenfassung für MainFrame
+	/// </summary>
+	public ref class MainFrame : public System::Windows::Forms::Form
+	{
+	public:
+		MainFrame(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Konstruktorcode hier hinzufügen.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		~MainFrame()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainFrame
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(919, 505);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"MainFrame";
+			this->ShowIcon = false;
+			this->Text = L"MainFrame";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
